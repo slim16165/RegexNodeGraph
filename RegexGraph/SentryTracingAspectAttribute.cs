4b381ec1a2e144cd5ffba3f4b1f7605e0587954f
Gianluigi Salvi <gianluigi.salvi@gmail.com>
2025-02-09 20:10:52 +0100

    Aggiunto Sentry

---
+++ RegexGraph/SentryTracingAspectAttribute.cs
@@ -0,0 +1,40 @@
+﻿using PostSharp.Aspects;
+using PostSharp.Serialization;
+using Sentry;
+using System;
+using System.Diagnostics;
+
+[PSerializable]
+public class SentryTracingAspect : OnMethodBoundaryAspect
+{
+    public override void OnEntry(MethodExecutionArgs args)
+    {
+        // Avvia una transazione di tracing con il nome del metodo
+        var transaction = SentrySdk.StartTransaction(args.Method.DeclaringType.FullName + "." + args.Method.Name, "method-execution");
+        args.MethodExecutionTag = transaction;
+
+        Debug.WriteLine($"[Sentry] Start tracing: {args.Method.DeclaringType.FullName}.{args.Method.Name}");
+    }
+
+    public override void OnExit(MethodExecutionArgs args)
+    {
+        if (args.MethodExecutionTag is ISpan transaction)
+        {
+            transaction.Finish(SpanStatus.Ok);
+        }
+
+        Debug.WriteLine($"[Sentry] Finished tracing: {args.Method.DeclaringType.FullName}.{args.Method.Name}");
+    }
+
+    public override void OnException(MethodExecutionArgs args)
+    {
+        if (args.MethodExecutionTag is ISpan transaction)
+        {
+            transaction.Finish(SpanStatus.InternalError);
+        }
+
+        SentrySdk.CaptureException(args.Exception);
+
+        Debug.WriteLine($"[Sentry] Error in: {args.Method.DeclaringType.FullName}.{args.Method.Name}: {args.Exception.Message}");
+    }
+}
